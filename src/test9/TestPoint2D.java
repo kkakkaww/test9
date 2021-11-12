b984cb1bd58f9e094b0f4d865cb8194388a0b362
kkakkaww <1715575996@qq.com>
2021-11-12 16:49:25 +0800

    程序清单9-5

---
+++ src/test9/TestPoint2D.java
@@ -0,0 +1,14 @@
+package test9;
+
+import java.awt.geom.Point2D;
+import java.util.Scanner;
+public class TestPoint2D {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double x1=0,y=0;
+		Point2D p1 = new Point2D(x1,y1);
+		System.out.println(p1.toString());
+	}
+
+}
